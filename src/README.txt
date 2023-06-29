158db94ddffb7c259ed7b32d8ed58fa3dc9d9365
dudoof <email@example.com>
2023-06-29 15:28:00 +0200

    init

---
+++ src/README.txt
@@ -0,0 +1,2 @@
+//from here
+https://fasterthanli.me/articles/understanding-rust-futures-by-going-way-too-deep
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// in `src/main.rs`
+
+#[tokio::main]
+async fn main() {
+    println!("Hello from a (so far completely unnecessary) async runtime");
+}
