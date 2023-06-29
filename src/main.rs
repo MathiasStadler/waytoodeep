771f1ee22151b313a2c0ebb19e8cd0f7ef6c9ea2
dudoof <email@example.com>
2023-06-29 15:43:24 +0200

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 // in `src/main.rs`
 
+use color_eyre::Report;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Report> {
+    setup()?;
+
     println!("Hello from a (so far completely unnecessary) async runtime");
+
+    Ok(())
+}
+
+fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info")
+    }
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    Ok(())
 }
